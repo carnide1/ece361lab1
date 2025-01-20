8de1275baee675c9c963bb35d0aa18a57c64a141
Joseph <joseph.carnide@mail.utoronto.ca>
2025-01-20 12:10:36 -0500

    [Part 1] - allowed for text entry when calling socket/deliver programs

--- deliver.c
+++ deliver.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]) {
+    if( argc < 3){
+        printf("Error - Both a Server Address and a Server Port Number are Required.\n");
+        return 0;
+    
+    } else {
+        printf("Server Address: %s\n", argv[1]);
+        printf("Server Port Number: %s\n", argv[2]);
+    }
+
     printf("Hello World! - deliver\n");
 
     return 0;
--- server.c
+++ server.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]) {
+    if (argc < 2) { 
+        printf("Error - Port Number Required.\n"); 
+        return 0;
+    } else { 
+        printf("Port Number: %s\n", argv[1]); 
+    }    
     printf("Hello World! - server\n");
+
+    //open a port at the number given by argv
+
+    //wait for a message
+
+    //condition check for the message
 
     return 0;
 }
